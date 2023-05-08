cc92ca9f9b65bceb88cc7d2f845132b05b77e1bf
Arvind <thakurarvindkr10@gmail.com>
2023-05-08 19:24:08 +0530

    added to add task func

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -2,6 +2,15 @@ import React, { useState } from 'react'
 
 const Todo = () => {
     const [task, setTask] = useState('')
+    const [tasks, setTasks] = useState([])
+    const addTask = (e) => {
+        if (task) {
+            const newTask = { id: new Date().getTime().toString(), title: task };
+            setTasks([...tasks, newTask])
+            localStorage.setItem('locatTasks', JSON.stringify([...tasks, newTask]));
+            setTask('')
+        }
+    }
     return (
         <div className='container row'>
             <h1 className='mt-3 text-white'>To-Do App</h1>
@@ -12,11 +21,20 @@ const Todo = () => {
                     value={task}
                     placeholder='write your task....'
                     className='form-control'
-                    onChange={(e)=>setTask(e.target.value)}
+                    onChange={(e) => setTask(e.target.value)}
                 />
             </div>
             <div className="col-4">
-                <button className="btn btn-primary form-control material-icons">➕</button>
+                <button onClick={addTask} className="btn btn-primary form-control material-icons">➕</button>
+            </div>
+            <div className="badge">
+                You have
+                {
+                    !tasks.length ? 'No tasks'
+                        : tasks.length == 1 ? 'One tasks'
+                            : tasks.length > 1 ? ` ${tasks.length} tasks`
+                                : null
+                }
             </div>
         </div>
     )
